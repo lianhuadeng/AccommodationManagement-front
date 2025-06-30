751d145a74272f7fdc14fa561c561749a31dbb26
lianhuadeng <2171822821@qq.com>
2025-06-30 09:54:56 +0800

    维修登记处理

--- src/api/repair.js
+++ src/api/repair.js
@@ -3,3 +3,7 @@ import request from '@/utils/request.js';
 export const myRepairService = () => {
     return request.get('/repair/myRepair')
 }
+
+export const submitRepairService = (data) => {
+    return request.post('/repair/add', data)
+}
